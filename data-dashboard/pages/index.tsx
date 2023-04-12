a08d447bfa5e0c6ab2e804fcceba2030965054dc
Antonio Monroy <tony_monroe1212@hotmail.com>
2023-04-12 16:38:16 -0600

    feat: deploy twoo

--- data-dashboard/pages/index.tsx
+++ data-dashboard/pages/index.tsx
@@ -1,6 +1,5 @@
-import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
-
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -13,7 +12,8 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <h1>Hello world</h1>
+		aaa
       </main>
     </>
-  )
+  );
 }
