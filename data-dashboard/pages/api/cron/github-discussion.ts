9244f22518465aa23fc340e9a9bc198f87e19db6
Antonio Monroy <tony_monroe1212@hotmail.com>
2023-04-13 11:26:33 -0600

    fix: hard code template in file

--- data-dashboard/pages/api/cron/github-discussion.ts
+++ data-dashboard/pages/api/cron/github-discussion.ts
@@ -1,8 +1,31 @@
-import { promises as fs } from "fs";
 import { NextApiRequest, NextApiResponse } from "next";
 
+const GH_GRAPHQL_ENDPOINT = "https://api.github.com/graphql";
+
 const GH_REPOSITORY_ID = "R_kgDOI-3SEw";
+
 const GENERAL_DISCUSSION_CATEGORY_ID = "DIC_kwDOI-3SE84CU2qi";
+
+const GH_STANDUP_BODY = `
+Share your daily standup update with the team here. Respond to this
+discussion before the standup starts in the following format:
+
+
+\`\`\
+**What did you achieve in the last 24 hours?**:
+ - 
+
+**What are your priorities for the next 24 hours?**:
+ - 
+
+**Blockers**:
+ -
+
+**Shoutouts**:
+ - @username for x
+\`\`\`
+
+`;
 
 const buildQuery = (repositoryId: string, title: string, body: string) => {
   return `
@@ -34,17 +57,16 @@ export default async function handler(_: NextApiRequest, res: NextApiResponse) {
     month: "long",
   });
 
-  const body = await fs.readFile(
-    process.cwd() + "/pages/api/cron/standupDiscussionTemplate.txt",
-    "utf-8"
-  );
-
   const graphqlQuery = JSON.stringify({
-    query: buildQuery(GH_REPOSITORY_ID, `Daily Standup - ${today}`, body),
+    query: buildQuery(
+      GH_REPOSITORY_ID,
+      `Daily Standup - ${today}`,
+      GH_STANDUP_BODY
+    ),
     variables: {},
   });
 
-  const response = await fetch("https://api.github.com/graphql", {
+  const response = await fetch(GH_GRAPHQL_ENDPOINT, {
     method: "POST",
     headers,
     body: graphqlQuery,
