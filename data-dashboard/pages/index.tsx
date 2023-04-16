a4511c7402d3658dd93c217d5ca7e57f6bde3647
Antonio Monroy <tony_monroe1212@hotmail.com>
2023-04-16 15:35:30 -0600

    feat: add airbnb config

--- data-dashboard/pages/index.tsx
+++ data-dashboard/pages/index.tsx
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import styles from '@/styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -14,5 +14,5 @@ export default function Home() {
         <h1>Hello world</h1>
       </main>
     </>
-  )
+  );
 }
