9559a0c6b7b737e4918dbd8e3285b2bd6417f87c
Antonio Monroy <tony_monroe1212@hotmail.com>
2023-04-27 17:36:59 -0600

    feat: add trpc boilerplate

---
+++ data-dashboard/server/routers/_app.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+import { procedure, router } from '../trpc';
+
+export const appRouter = router({
+  hello: procedure
+    .input(
+      z.object({
+        text: z.string(),
+      }),
+    )
+    .query(({ input }) => ({
+      greeting: `hello ${input.text}`,
+    })),
+});
+// export type definition of API
+export type AppRouter = typeof appRouter;
