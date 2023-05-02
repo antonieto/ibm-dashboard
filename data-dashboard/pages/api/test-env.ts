7148620be38471b41020677cbe997346757df7fb
Antonio Monroy <tony_monroe1212@hotmail.com>
2023-05-02 14:20:32 -0600

    ref: remove test endpoint

--- data-dashboard/pages/api/test-env.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiHandler } from 'next';
-
-const handler: NextApiHandler = async (req, res) => res.json({
-  env: process.env,
-  another_test: process.env.DATABASE_URL,
-});
-
-export default handler;
