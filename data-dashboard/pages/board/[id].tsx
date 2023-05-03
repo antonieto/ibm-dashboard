44b2ac0e6babe5919cffc1f84662e32ad50f914d
Int-Angel <padilla.angel88@gmail.com>
2023-05-03 07:18:16 -0600

    feat - Created ButtonWithIcon component

--- data-dashboard/pages/board/[id].tsx
+++ data-dashboard/pages/board/[id].tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import styled from 'styled-components';
+import { Add } from '@carbon/icons-react';
 
 import { Responsive, WidthProvider } from 'react-grid-layout';
 import 'react-grid-layout/css/styles.css';
@@ -8,6 +9,7 @@ import 'react-resizable/css/styles.css';
 import { NavBar } from '../../lib/components';
 import { MOCK_CHART_LIST } from './MOCK_CHART_LIST';
 import ChartCard from '../../lib/components/ChartCard/ChartCard';
+import ButtonWithIcon from '../../lib/components/ButtonWithIcon/ButtonWithIcon';
 
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 const breakpoints = {
@@ -37,6 +39,12 @@ const margin: [number, number] | { [P: string]: [number, number] } = {
 const Container = styled.div`
   background-color: #f4f5f5;
   height: 100vh;
+`;
+
+const AddButtonContainer = styled.div`
+  position: absolute;
+  bottom: 20px;
+  left: 20px;
 `;
 
 export default function Board(): JSX.Element {
@@ -87,6 +95,9 @@ export default function Board(): JSX.Element {
           ))}
         </ResponsiveReactGridLayout>
       </div>
+      <AddButtonContainer>
+        <ButtonWithIcon text="Add Widget" icon={Add} />
+      </AddButtonContainer>
     </Container>
   );
 }
