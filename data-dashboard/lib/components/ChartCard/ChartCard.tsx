d9794feebd8721fd33d7570bafc98be3322ae7f0
Angel Padilla <56850703+Int-Angel@users.noreply.github.com>
2023-05-04 16:33:23 -0500

    Feat/board grid (#165)
    * feat - base grid layout
    
    * feat - ChartCard component implemented
    
    * feat - Created ButtonWithIcon component
    
    * feat - Created ButtonWithIcon component
    
    * feat - selected state chart card
    
    * feature- chart card menu implemented
    
    * Solved build problem
    
    * Solved build problem

---
+++ data-dashboard/lib/components/ChartCard/ChartCard.tsx
@@ -0,0 +1,132 @@
+import styled from 'styled-components';
+import {
+  useEffect,
+  useRef,
+  useState,
+  ReactNode,
+} from 'react';
+import { Settings, SettingsAdjust } from '@carbon/icons-react';
+import { Title, Subtitle } from '@tremor/react';
+
+import 'react-grid-layout/css/styles.css';
+import 'react-resizable/css/styles.css';
+
+interface Props {
+  children: ReactNode;
+}
+
+const CardComponent = styled.div<{ readonly selected: boolean }>`
+  background-color: white;
+  border: none;
+  cursor: pointer;
+
+  height: 100%;
+  width: 100%;
+
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 4px;
+
+  border: ${(props) => (props.selected ? '2px solid #0f62fe !important' : '')};
+
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+
+  padding: 20px;
+
+  position: relative;
+  z-index: 0;
+`;
+// background-color: #d2d3d4;
+const SettingsContainer = styled.div`
+  position: absolute;
+  bottom: -20px;
+  right: 10px;
+  background-color: #fafafa;
+  border-radius: 50px;
+
+  height: fit-content;
+  width: fit-content;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  z-index: 10;
+
+  gap: 5px;
+  padding-left: 10px;
+  padding-right: 10px;
+
+  border: 2px solid #0f62fe;
+
+  overflow: hidden;
+`;
+
+const SettingsIcon = styled.div`
+  border-radius: 50%;
+  padding: 5px;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    background-color: #0f62fe;
+    color: white;
+  }
+
+  &:active {
+    background-color: #002d9c;
+  }
+`;
+
+const Line = styled.div`
+  height: 20px;
+  border-left: 2px solid #d2d3d4;
+`;
+
+export default function ChartCard({ children }: Props): JSX.Element {
+  const [selected, setSelected] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  const handleClick = () => {
+    setSelected(!selected);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        setSelected(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [ref]);
+
+  return (
+    <CardComponent selected={selected} onClick={handleClick} ref={ref}>
+      <Title>title</Title>
+      <Subtitle>
+        The IUCN Red List has assessed only a small share of the total known
+        species in the world.
+      </Subtitle>
+      {children}
+      {selected && (
+        <SettingsContainer
+          onClick={(e: any) => {
+            e.stopPropagation();
+          }}
+        >
+          <SettingsIcon>
+            <Settings aria-label="Add" size={24} />
+          </SettingsIcon>
+          <Line />
+          <SettingsIcon>
+            <SettingsAdjust aria-label="Add" size={24} />
+          </SettingsIcon>
+        </SettingsContainer>
+      )}
+    </CardComponent>
+  );
+}
