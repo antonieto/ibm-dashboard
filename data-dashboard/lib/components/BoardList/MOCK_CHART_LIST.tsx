7f3d8a8b55e5045911e7d05413231ac7f1c580e5
Angel Padilla <56850703+Int-Angel@users.noreply.github.com>
2023-05-04 16:33:23 -0500

    Feat/board grid (#165)
    * feat - base grid layout
    
    * feat - ChartCard component implemented
    
    * feat - Created ButtonWithIcon component
    
    * feat - Created ButtonWithIcon component
    
    * feat - selected state chart card
    
    * feature- chart card menu implemented
    
    * Solved build problem
    
    * Solved build problem

---
+++ data-dashboard/lib/components/BoardList/MOCK_CHART_LIST.tsx
@@ -0,0 +1,41 @@
+export const MOCK_CHART_LIST = [
+  {
+    i: 'widget1',
+    x: 0,
+    y: 0,
+    w: 2,
+    h: 3,
+  },
+  {
+    i: 'widget2',
+    x: 2,
+    y: 0,
+    w: 2,
+    h: 3,
+  },
+  {
+    i: 'widget3',
+    x: 4,
+    y: 0,
+    w: 2,
+    h: 3,
+  },
+  {
+    i: 'widget4',
+    x: 6,
+    y: 0,
+    w: 2,
+    h: 3,
+  },
+  {
+    i: 'widget5',
+    x: 0,
+    y: 3,
+    w: 2,
+    h: 3,
+  },
+];
+
+export default {
+  MOCK_CHART_LIST,
+};
