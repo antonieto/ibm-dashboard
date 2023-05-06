5f5cfa5ce30cff6c554dee1e6831e304a470ec11
Antonio Monroy <tony_monroe1212@hotmail.com>
2023-05-05 22:43:03 -0600

    feat: add signup and login routes

--- data-dashboard/server/trpc/context.ts
+++ data-dashboard/server/trpc/context.ts
@@ -11,6 +11,7 @@ export const createContextInner = async () => {
 
 export const createContext = async (opts: CreateNextContextOptions) => {
   const innerContext = await createContextInner();
+
   return {
     ...innerContext,
     req: opts.req,
